f5a99174e1085d5ce22af00e65a63c931c95d3e8
Администратор <nuriza.samidinovna@gmail.com>
2020-02-07 21:29:46 +0600

    added jdbc product and DayStatistic

--- src/main/java/com/model/DayStatistic.java
+++ src/main/java/com/model/DayStatistic.java
@@ -1,5 +1,15 @@
 package com.model;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
 public class DayStatistic {
     int id;
     String userLogin;
